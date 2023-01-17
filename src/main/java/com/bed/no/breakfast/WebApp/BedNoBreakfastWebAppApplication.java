434fe4380d4081f63f07eb1a2383491ab1460309
athi <aonlysmith@gmail.com>
2023-01-17 13:37:06 +0200

    Added skeleton code

---
+++ src/main/java/com/bed/no/breakfast/WebApp/BedNoBreakfastWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.bed.no.breakfast.WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedNoBreakfastWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BedNoBreakfastWebAppApplication.class, args);
+	}
+
+}
